a2cf3440fddd9799056717e43ae0fb9689765c57
Wen-Lung, Tsai <55378870+tsai1247@users.noreply.github.com>
2021-05-09 11:32:45 +0800

    [Debug] Fxiing the crash when frequently run it.

--- IGP.py
+++ IGP.py
@@ -5,50 +5,52 @@ from platform import system as plf_sys
 from wx import App, Frame, ID_ANY, FileDialog, FD_OPEN, FD_FILE_MUST_EXIST
 
 def processImg(outputType = 0, filepath = '', BlackWriteLine = 200):
+    
     img = open(filepath)
-    img = img.convert('RGB')
-    newimg = []
+    try:
+        img = img.convert('RGB')
+        newimg = []
 
-    process = 0
-    for i in range(img.width):
-        newimg.append([])
-        if( i/img.height>=process/10 ):
-            print(process*10, '%')
-            process+=1
+        process = 0
+        for i in range(img.width):
+            newimg.append([])
+            if( i/img.height>=process/10 ):
+                print(process*10, '%')
+                process+=1
 
 
-        for j in range(img.height):
-            coord = i, j
-            newimg[len(newimg)-1].append([])
-            cur = 0
-            r, g, b = img.getpixel(coord)
-            cur = r+g+b
-            if outputType==1:
-                for k in range(3):
-                    color = ta, tb, tc = int(cur/3), int(cur/3), int(cur/3)
-                    img.putpixel(coord, color)
-            elif outputType==2:
-                if cur/3>BlackWriteLine:
+            for j in range(img.height):
+                coord = i, j
+                newimg[len(newimg)-1].append([])
+                cur = 0
+                r, g, b = img.getpixel(coord)
+                cur = r+g+b
+                if outputType==1:
                     for k in range(3):
-                        color = ta, tb, tc = 255, 255, 255    
+                        color = ta, tb, tc = int(cur/3), int(cur/3), int(cur/3)
                         img.putpixel(coord, color)
-                else:
-                    for k in range(3):
-                        color = ta, tb, tc = 0, 0, 0
-                        img.putpixel(coord, color)
-            
+                elif outputType==2:
+                    if cur/3>BlackWriteLine:
+                        for k in range(3):
+                            color = ta, tb, tc = 255, 255, 255    
+                            img.putpixel(coord, color)
+                    else:
+                        for k in range(3):
+                            color = ta, tb, tc = 0, 0, 0
+                            img.putpixel(coord, color)
+                
 
-    print('100%')
-    print('Saving Image...')
+        print('100%')
+        print('Saving Image...')
+        img.save('output{}.png'.format(BlackWriteLine))
 
-
-    img.save('output{}.png'.format(BlackWriteLine))
-
+    finally:
+        img.close()
+        
     if plf_sys() == 'Windows':
         os_sys('start {}'.format('output{}.png'.format(BlackWriteLine)))
 
     print('Done')
-
 
 app = App()
 frame = Frame(
